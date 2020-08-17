2b78fa03f9434d5d2183e9278f9bf9e775ecf834
Sanchit Dang <sanchitd5@live.com>
2020-08-17 12:49:18 +1000

    Add Title to media files

--- src/components/dependants/VideoManager.js
+++ src/components/dependants/VideoManager.js
@@ -1,13 +1,22 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState, useCallback } from 'react';
 import PropTypes from "prop-types";
 import { UploadManagerContext } from "contexts";
-import { Grid, Card, CardMedia, CardActions, Button, Divider, CardHeader } from "@material-ui/core";
+import { Grid, Card, CardMedia, CardActions, Button, Divider, CardHeader, TextField, } from "@material-ui/core";
 import { API } from "helpers";
 import { notify } from "components";
-import { LoadingScreen } from 'components/common/Loading';
+import { LoadingScreen, EnhancedModal } from 'components';
+import { TextHelper } from 'helpers/index';
 
 const MediaCard = props => {
-  const { link, type, isUploaded, id, setMedia, resolution, thumb } = props;
+  const { link, type, isUploaded, id, setMedia, resolution, thumb, title } = props;
+  const [updatedTitle, setUpdatedTitle] = useState(title || "");
+  const [isEditOpen, setIsEditOpen] = useState(false);
+
+  const updateMedia = useCallback(() => {
+    API.getMediaFiles((response) => {
+      setMedia(response);
+    });
+  }, [setMedia]);
 
   const renderMedia = () => {
     if (!isUploaded) {
@@ -25,24 +34,61 @@ const MediaCard = props => {
     }
   };
 
+  let editTitleContent = (
+
+    <Grid container spacing={1} style={{ marginTop: "2px" }}>
+      <Grid item xs={12}>
+        <TextField required label="Title" fullWidth={true} variant="outlined" defaultValue={title} value={updatedTitle} onChange={(e) => {
+          setUpdatedTitle(e.target.value);
+        }} />
+      </Grid>
+    </Grid>
+  );
+
   return (
     <Card style={{ height: "100%" }}>
-      <CardHeader subheader={"Resolution : " + resolution + "p"} />
+      <EnhancedModal
+        isOpen={isEditOpen}
+        dialogTitle="Edit Title"
+        dialogContent={editTitleContent}
+        options={{
+          submitButtonName: "Save",
+          onSubmit: () => {
+            API.updateMediaTitle({
+              _id: id,
+              title: updatedTitle
+            }, (response) => {
+              setIsEditOpen(false);
+              if (response === true)
+                updateMedia();
+            });
+          },
+          onClose: () => {
+            setIsEditOpen(false);
+          }
+        }}
+      />
+      <CardHeader
+        title={title ? TextHelper.titleCase(title) : "No Title Found"}
+        subheader={resolution ? `Resolution : ${resolution}p` : undefined} />
       <Divider />
       {
         renderMedia()
       }
       <CardActions>
-        {props.onSelect && <Button size="small" color="primary" onClick={() => {
+        {props.onSelect ? <Button variant="contained" size="small" color="primary" onClick={() => {
           props.onSelect({ link, type, isUploaded, resolution, thumb });
         }}>
           Select
+        </Button> : <Button variant="contained" size="small" color="primary" onClick={() => {
+          setIsEditOpen(true);
+        }}>
+            Edit Title
         </Button>}
-        <Button size="small" color="secondary" onClick={() => {
+
+        <Button variant="contained" size="small" color="secondary" onClick={() => {
           API.deleteVideo(id, () => {
-            API.getMediaFiles((response) => {
-              setMedia(response);
-            });
+            updateMedia();
           });
         }}>
           Delete
@@ -60,7 +106,8 @@ MediaCard.propTypes = {
   id: PropTypes.string.isRequired,
   resolution: PropTypes.string.isRequired,
   onSelect: PropTypes.func,
-  thumb: PropTypes.string
+  thumb: PropTypes.string,
+  title: PropTypes.string
 };
 
 
@@ -109,7 +156,7 @@ export const VideoManager = props => {
     </Grid>
   </Grid>{media === undefined ? <LoadingScreen /> : media.length > 0 ? <Grid container style={{ padding: "10px" }} spacing={1}>
     {media.map((m, i) => <Grid key={"item_" + i} item xs={12} sm={12} lg={4} md={4} xl={4}>
-      <MediaCard link={m.link} type={m.type} isUploaded={m.isUploaded} id={m._id} setMedia={setMedia} resolution={m.resolution} onSelect={props.onSelect} thumb={m.thumb} />
+      <MediaCard link={m.link} title={m.title} type={m.type} isUploaded={m.isUploaded} id={m._id} setMedia={setMedia} resolution={m.resolution} onSelect={props.onSelect} thumb={m.thumb} />
     </Grid>)}
   </Grid> : <div style={{ textAlign: "center", paddingTop: "30px" }}> No Media Avaialble</div>}
   </>);
